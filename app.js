004399e19e45a9beeb789238145b74022f9ceda6
Ilya Ivchenkov <gunti911@gmail.com>
2022-05-29 22:59:36 +0300

    refactor: names of identifiers and classes according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("edit-task");
+    //If class of the parent is .edit-task
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .edit-task
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .edit-task on the parent.
+    listItem.classList.toggle("edit-task");
 };
 
 
@@ -192,4 +192,4 @@ for (var i=0; i<completedTasksHolder.children.length;i++){
 
 //prevent creation of empty tasks.
 
-//Change edit to save when you are in edit mode.
+//Change edit to save when you are in edit task.
